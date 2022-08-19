35064c81db89ea9b8bee0ff9f3254696fc5b2fc7
Richard-creator10 <111611782+Richard-creator10@users.noreply.github.com>
2022-08-19 19:15:34 -0300

    feat: add new variables for rules of the game

---
+++ script/mock.js
@@ -0,0 +1,5 @@
+// var parent = document.getElementById("parent");
+// var child_nodes = parent.childNodes;
+// console.log(child_nodes.length); // let's assume "2"
+// parent.appendChild(document.createElement("div"));
+// console.log(child_nodes.length); // should output "3"
